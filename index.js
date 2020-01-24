b272e01600178eac6e11866bb3382980dce7112c
Eugene-im <em.92@mail.ru>
2020-01-25 00:43:31 +0200

    fix

--- index.js
+++ index.js
@@ -20,6 +20,43 @@ http.createServer(function (req, res) {
     }
 }).listen(3000);
 
+// var 1
+
+// const express = require('express');
+// const app = express();
+
+// var x = "vasia";
+// var y = "petia";
+
+// app.listen(3000)
+
+// app.use(express.static('public'));
+// // app.get('/static',express.static('public'))
+// app.get('/ajax', function(req, res){
+//     console.log("/ajax get");
+//     res.end(x);
+// })
+
+// app.get('/asd1', function(req, res){
+//     console.log("/asd1 get");
+//     var dat = new Date();
+//     res.end(y)
+//     // res.end(dat.toString());
+// })
+
+// app.post('/asd1', function(req, res){
+//     // console.log("/asd1 post");
+//     // var dat = new Date();
+//     express.static('public')
+//     res.end(dat.toString());
+// })
+
+// app.post('/*', function(req, res){
+//     console.log("/asd1 post");
+//     var dat = new Date();
+//     res.end(dat.toString());
+// })
+
 
 // variant 2
 
